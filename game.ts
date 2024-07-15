d2d79c45d7b4633fb6ba055e1e9658fab8ef9029
Mehmood <92mehmood313@gmail.com>
2024-07-15 22:00:26 +0500

    number guessing game

---
+++ game.ts
@@ -0,0 +1,24 @@
+const randomNumber:Number = Math.floor(Math.random() * 9 +1);
+
+console.log("\n--Number Guessing Game--\n")
+
+import inquirer from "inquirer";
+const userNumber:Number = await inquirer.prompt([
+    {
+    "name": "userNumber",
+    "type": "number",
+    "message": "Guess a number between 1 to 9"
+    }
+]);
+
+if(userNumber === randomNumber){
+    console.log("Your Number: ",userNumber,"\nComputer Number: ",randomNumber)
+    console.log("Congratulations! You win")
+}
+else if(userNumber !== randomNumber){
+    console.log("Your Number: ",userNumber,"\nComputer Number: ",randomNumber)
+    console.log("You lose!")
+}
+else{
+    console.log("Invalid input")
+}
